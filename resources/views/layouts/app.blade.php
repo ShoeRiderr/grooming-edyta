f4fc889b937bc3588867d771c33d10973dff0354
ShoeRiderr <kamilsocha20@gmail.com>
2021-08-02 22:16:24 +0200

    create navbar with logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,32 @@
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <a href="/">
-                    <img src=""> {{-- logo --}}
-                </a>
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark flex-column align-items-stretch bg">
+                <div class="d-flex">
+                    <a href="#" class="navbar-brand mx-sm-auto mr-auto" style="font-size:1.8em;">
+                        <img class="responsive" src="data:image/png;base64,{{ base64_encode(file_get_contents(resource_path('images/logo.png'))) }}">
+                    </a>
+                    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse w-100 sticky-top" id="navbarMenu"  style="font-size:1.2em;">
+                    <ul class="navbar-nav mx-sm-auto">
+                        <li class="nav-item">
+                            <p><a href="#" class="nav-link">Hotel dla psów</a></p>
+                        </li>
+                        <li class="nav-item">
+                            <p><a href="#" class="nav-link">Grooming</a></p>
+                        </li>
+
+                        <li class="nav-item">
+                            <p><a href="#" class="nav-link">Fizjoterapia</a></p>
+                        </li>
+                        <li class="nav-item">
+                            <p><a href="#" class="nav-link">Kontakt</a></p>
+                        </li>
+                    </ul>
+                </div>
             </nav>
 
              @yield('content')
