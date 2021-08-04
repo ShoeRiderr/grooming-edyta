2a45c2ed904452ef3d029c4d30c2ed2053d7fc35
ShoeRiderr <kamilsocha20@gmail.com>
2021-08-04 19:19:19 +0200

    refactor logo background

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -1,3 +1,14 @@
 @extends('layouts.app')
 
-@section('title', "Edyta Groomer")
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                dds
+            </div>
+            <div class="col-md-6">
+                dsfs
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/grooming.blade.php
+++ resources/views/grooming.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-@section('title', "Edyta Groomer")
-
 @section('content')
 
 @endsection
